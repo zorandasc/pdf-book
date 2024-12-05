939fef5e659609e2612a46e0ffc0354e5748abb0
Zoran Dasic <zorandsc@yahoo.com>
2024-12-05 15:08:56 +0100

    final update

--- app/page.js
+++ app/page.js
@@ -1,32 +1,41 @@
 "use client";
+//DEFINISANO react-pdf DOKUMENTACIJOM
 import "react-pdf/dist/esm/Page/TextLayer.css";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
+
 import React, { useState, useRef } from "react";
 import Image from "next/image";
 
+//BIBLIOTEKA ZA FLIP BOOK ANIMACIJU
 import HTMLFlipBook from "react-pageflip";
-import { pdfjs, Document, Page as ReactPdfPage } from "react-pdf";
-import dynamic from "next/dynamic"; // Import dynamic from Next.js
 
+//BIBLIOTEKA ZA HANDLOVANJE PDF
+import { pdfjs, Document, Page as ReactPdfPage } from "react-pdf";
+
+import dynamic from "next/dynamic"; // Import dynamic from Next.js
 // Dynamically import PinchZoomPan with SSR disabled
+//ODNOSNO IMPORTUJ PinchZoomPan KOMPONENTU SAMO U BROWSERU
 const PinchZoomPan = dynamic(() => import("react-responsive-pinch-zoom-pan"), {
   ssr: false,
 });
 
+//NESTO DEFINISANO react-pdf DOKUMENTACIJOM
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   "pdfjs-dist/build/pdf.worker.min.mjs",
   import.meta.url
 ).toString();
 
 const backgroundImage = "/back1.jpg";
+//PDF
 const samplePDF = "/zekicom.pdf";
+//ICONICE
 const magplus = "/magplus.svg";
-const magminus = "/magminus.svg";
 const arrow = "/arrow.svg";
 
 const width = 400;
 const height = 600;
 
+//JEDNA PDF STRANICA
 const Page = React.forwardRef(({ pageNumber }, ref) => {
   return (
     <div ref={ref} style={{ marginBottom: "10px" }}>
@@ -39,20 +48,24 @@ Page.displayName = "Page";
 
 const Home = () => {
   const [isZoomMode, setIsZoomMode] = useState(false); // Toggle zoom mode
-  const [currentPage, setCurrentPage] = useState(0); // Track current page
+  const [currentPage, setCurrentPage] = useState(0); // Track current page - SACUVAJ KLIKNUTU STRANICU
   const flipBookRef = useRef(null); // Ref to access the FlipBook component
 
+  //ULAZ U ZOOM MODE
   const handleEnterZoomMode = () => {
     setIsZoomMode(true);
   };
 
+  //IZLAZ IZ ZOOM MODA
   const handleExitZoomMode = () => {
     setIsZoomMode(false);
     if (flipBookRef.current) {
+      //KAD SE VRATIS IZ PINCHZOOMA VRATI flipBook TREBA DA POCNE OD KLIKNUTOG PAGEA
       flipBookRef.current.pageFlip().flip(currentPage); // Flip to the correct page
     }
   };
 
+  //SVAKI PUT KAD KLIKNES PDF STRANICU SACUVAJ BROJ STRANICE U STATE
   const handlePageFlip = (e) => {
     setCurrentPage(e.data); // `e.data` contains the current page index
     console.log("Current Page:", e.data);
@@ -75,7 +88,6 @@ const Home = () => {
         overflow: "hidden",
       }}
     >
-      {/* FlipBook Component*/}
       <div
         style={{
           position: "relative",
@@ -83,9 +95,10 @@ const Home = () => {
           height: `${height}px`,
         }}
       >
+        {/* FlipBook Component - KOMPONENTA SA SVIM PDF STRANICAMA*/}
         <div
           style={{
-            visibility: isZoomMode ? "hidden" : "visible", // Hide FlipBook visually
+            visibility: isZoomMode ? "hidden" : "visible", // Hide FlipBook visually WITH CSS
             position: "absolute",
             top: 0,
             left: 0,
@@ -108,7 +121,7 @@ const Home = () => {
           </Document>
         </div>
 
-        {/* PinchZoomPan Component*/}
+        {/* PinchZoomPan Component KOMPONENTA SAMO SA ZUMIRANOM STRANICOM*/}
         <div
           style={{
             visibility: isZoomMode ? "visible" : "hidden", // Hide PinchZoomPan visually
